91762f0178fc983193c06f3ece2c9df0c539bfe0
siyu0927 <yesfish0927@gmail.com>
2022-11-02 15:52:54 +0800

    siyu

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+/*
+17~20 放第一個數字
+21~24 放第二個數字
+28~32 迴圈加法
+33~34 結果,放回RAM[2]
+*/
+
+@2
+D=A
+@0
+M=D
+@4
+D=A
+@1
+M=D
+@11
+M=D
+D=0
+@0
+D=D+M
+@11
+M=M-1
+M;JNE
+@2
+M=D
+@18
+0;JMP
