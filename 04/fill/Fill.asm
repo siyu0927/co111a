7029b85ea4868d94c14c060e41b4327a43e720e0
siyu0927 <yesfish0927@gmail.com>
2022-11-02 15:27:14 +0800

    siyu

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+/*
+16384~24575螢幕控制,24576鍵盤輸入
+23~26 判斷鍵盤有沒有輸入  39~42 判斷鍵盤是否放開
+27~30 判斷何時回圈要停    43~46 判斷迴圈何時停
+31~38 螢幕迴圈變黑        47~54 螢幕迴圈變白
+55~56 結束一套,回原點
+*/
+
+@24576
+D=M
+@0
+D;JEQ
+@8192
+D=A
+@10
+M=D
+@16384
+D=A
+A=D
+D=D+1
+M=-1
+@10
+M=M-1
+M;JNE
+@24576
+D=M
+@16
+D;JNE
+@8192
+D=A
+@26
+M=D
+@16384
+D=A
+A=D
+D=D+1
+M=0
+@26
+M=M-1
+M;JNE
+@0
+0;JMP
+
+
+
